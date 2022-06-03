390a9b686e3729983ac7fab726f3ec3e3c490979
kamit17 <kamit17@outlook.com>
2022-06-03 14:16:03 -0500

    Go

---
+++ lcgolandbootcamp/Go/constants.go
@@ -0,0 +1,14 @@
+//A constant in Go programming language is declared by using the keyword “const”
+
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	const b = 10
+	fmt.Println(b)
+	b = 30
+	fmt.Println(b)
+}
