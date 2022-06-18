78ede3282de09912831fc2fb78b928e4bf83cf43
kamit17 <kamit17@outlook.com>
2022-06-17 22:11:26 -0500

    Slices

---
+++ Projects/src/CalliCoder/append_slice_to_slice.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	slice1 := []string{"Jack", "John", "Peter"}
+	slice2 := []string{"Bill", "Mark", "Steve"}
+
+	//append one slice to another using the ... operator
+	slice3 := append(slice1, slice2...)
+
+	fmt.Println("slice1 = ", slice1)
+	fmt.Println("slice2 = ", slice2)
+	fmt.Println("After appending slice1 & slice2 = ", slice3)
+}
