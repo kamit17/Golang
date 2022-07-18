17325eb9c1df622fa198e37ca3a06c16b8c80c62
kamit17 <kamit17@outlook.com>
2022-07-18 13:34:07 -0500

    pointer initilization

---
+++ Projects/src/CalliCoder/pointers/pointer initilization.go
@@ -0,0 +1,14 @@
+// A pointer van be initialized with the memory address of another variable. The address of the variable can be retreived usng the & operator
+
+package main
+
+import "fmt"
+
+func main() {
+	var a = 5.67
+	var p = &a
+
+	fmt.Println("Value stored in variable a = ", a)
+	fmt.Println("Address of variable a = ", &a)
+	fmt.Println("Value stored in variable p = ", p)
+}
