abf6ea181c3f1f07f9ce83eece826e0a3836ae16
kamit17 <kamit17@outlook.com>
2022-07-28 09:55:49 -0500

    Structs are value types

---
+++ Projects/src/CalliCoder/structs/structs_vare_value_types.go
@@ -0,0 +1,25 @@
+/*Structs are value types. When you assign one struct variable to another, a new copy of the struct is created and assigned. Similarly, when you pass a struct to another function, the function gets its own copy of the struct
+ */
+
+package main
+
+import "fmt"
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+func main() {
+	//Structs are value types.
+	p1 := Point{10, 20}
+	pr := p1 //A copy of the struct 'p1' is assigned to 'p2'
+	fmt.Println("p1 =  ", p1)
+	fmt.Println("p2 = ", p2)
+
+	p2.X = 15
+	fmt.Println("\nAfter modifying p2:")
+	fmt.Println("p1 = ", p1)
+	fmt.Println("p2 = ", p2)
+
+}
